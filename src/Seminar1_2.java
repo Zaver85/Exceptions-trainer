45d973516bbd90aa9d4590ed9f9f6a9688ece48e
Zaver85 <Zaver85.com@mail.ru>
2024-10-18 15:17:02 +0700

    add task

---
+++ src/Seminar1_2.java
@@ -0,0 +1,41 @@
+/*Реализуйте метод, принимающий в качестве аргумента целочисленный двумерный массив.
+Необходимо посчитать и вернуть сумму элементов этого массива.
+При этом накладываем на метод 2 ограничения: метод может работать только с
+квадратными массивами (кол-во строк = кол-ву столбцов), и в каждой ячейке может лежать
+только значение 0 или 1.
+Если нарушается одно из условий, метод должен бросить RuntimeException с сообщением об
+ошибке.*/
+
+
+public class Seminar1_2 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 0, 1},{1, 1, 1},{0, 0, 1}};
+        int sum = sumElement(matrix);
+        System.out.println(sum);
+
+    }
+
+    public  static int sumElement(int[][] matrix) {
+        int n = matrix.length;
+        for (int[] row : matrix) {
+            if (row.length != n){
+                throw new RuntimeException("Количество сторок не равно количеству столбцов");
+            }
+        }
+        for (int[] row : matrix) {
+            for (int element : row) {
+                if (element != 0 && element != 1) {
+                    throw new RuntimeException("Массив должен содержать только 0 или 1");
+                }
+            }
+        }
+            int sum =0;
+            for (int[] row : matrix) {
+                for (int element : row) {
+                    sum += element;
+                }
+            }
+            return sum;
+        }
+    }
+
