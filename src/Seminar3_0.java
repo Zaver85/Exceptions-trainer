540570cd59be41b9959dec8c06ea118cdc0c82a6
Zaver85 <Zaver85.com@mail.ru>
2024-11-07 15:14:32 +0700

    create 3_0

---
+++ src/Seminar3_0.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Seminar3_0 {
+    public static void main(String[] args) {
+
+    }
+
+    public void rwLine(Path pathRead, Path pathWrite) throws IOException {
+        try (BufferedReader in = Files.newBufferedReader(pathRead);
+             BufferedWriter out = Files.newBufferedWriter(pathWrite)) {
+            out.write(in.readLine());
+        }
+    }
+}
+
