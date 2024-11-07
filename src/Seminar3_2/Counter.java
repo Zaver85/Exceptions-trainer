c730be09f8f3f4c8646fe2312a3cfaffaf481e96
Zaver85 <Zaver85.com@mail.ru>
2024-11-07 15:28:21 +0700

    create 3_2

---
+++ src/Seminar3_2/Counter.java
@@ -0,0 +1,23 @@
+package Seminar3_2;
+
+import java.io.IOException;
+
+public class Counter implements AutoCloseable{
+    private  int count;
+    private  boolean closed = false;
+    public void add() throws IOException {
+        if (closed){
+            throw  new IOException();
+        }
+        count++;
+    }
+
+    public String toString(){
+        return String.valueOf(count);
+    }
+
+    @Override
+    public void close() throws Exception {
+        closed = true;
+    }
+}
